f57f57f96f46f32daac87bd009bb1478e6ea9a4b
VladKing12 <kohelev04@gmail.com>
2022-05-18 22:12:29 +0300

    Add memory and gdt init

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,9 +2,16 @@
 
 #include <vga/tty.h>
 #include <gdt/gdt.h>
+#include <memory/memory.h>
+
+extern uint32_t kernel_end;
+extern uint32_t kernel_base;
 
 void kernel_main(void) {
 	terminal_initialize();
     printf("Hello, world! %x \n",0x12ff);
-	//gdt_init();
+    mm_init(&kernel_end);
+    printf("MM initialized\n");
+	gdt_init();
+    printf("GDT initialized\n");
 }
