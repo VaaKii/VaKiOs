055582574acf431ac8022533524cb4af3c76f8cc
VladKing12 <kohelev04@gmail.com>
2022-05-18 22:14:49 +0300

    ADD: Add panic command

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -9,6 +9,8 @@
 extern "C" {
 #endif
 
+#define panic(...) {printf("***KERNEL PANIC*** in %s at line %d in function: %s\n", __FILE__, __LINE__, __func__); printf(__VA_ARGS__); for(;;);}
+
 int printf(const char* __restrict, ...);
 int putchar(int);
 void __itoa(unsigned i,unsigned base,char* buf);
