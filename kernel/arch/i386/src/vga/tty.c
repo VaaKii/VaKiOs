503b857a597bfca7f64b70367b08ed9731eb8033
VladKing12 <kohelev04@gmail.com>
2022-05-18 22:16:08 +0300

    ADD: Added cleaning terminal to another function

--- kernel/arch/i386/src/vga/tty.c
+++ kernel/arch/i386/src/vga/tty.c
@@ -16,17 +16,21 @@ static size_t terminal_column;
 static uint8_t terminal_color;
 static uint16_t* terminal_buffer;
 
+void clear_terminal() {
+    terminal_buffer = VGA_MEMORY;
+    for(size_t y = 0; y < VGA_HEIGHT; y++) {
+        for (size_t x = 0; x < VGA_WIDTH; x++) {
+            const size_t index = y * VGA_WIDTH + x;
+            terminal_buffer[index] = vga_entry(' ', terminal_color);
+        }
+    }
+}
+
 void terminal_initialize(void) {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
-	terminal_buffer = VGA_MEMORY;
-	for (size_t y = 0; y < VGA_HEIGHT; y++) {
-		for (size_t x = 0; x < VGA_WIDTH; x++) {
-			const size_t index = y * VGA_WIDTH + x;
-			terminal_buffer[index] = vga_entry(' ', terminal_color);
-		}
-	}
+	clear_terminal();
 }
 
 void terminal_setcolor(uint8_t color) {
@@ -47,8 +51,10 @@ void terminal_putchar(char c) {
             terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
             if (++terminal_column == VGA_WIDTH) {
                 terminal_column = 0;
-                if (++terminal_row == VGA_HEIGHT)
+                if (++terminal_row == VGA_HEIGHT){
                     terminal_row = 0;
+                    clear_terminal();
+                }
             }
         }
     }
